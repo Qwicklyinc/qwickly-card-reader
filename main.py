f142cb7e241e636458bcca5efca47eda44114dc2
Bartosz Przybysz <bartosz.d.przybysz@gmail.com>
2019-08-02 16:29:41 -0400

    Replace tabs with spaces

--- main.py
+++ main.py
@@ -18,56 +18,56 @@ session = requests.Session()
 
 
 def is_connected():
-	# Ping a DNS server to check connection
-	return os.system('ping 8.8.8.8 -c 1 -w 1 -q') == 0
-	
+    # Ping a DNS server to check connection
+    return os.system('ping 8.8.8.8 -c 1 -w 1 -q') == 0
+    
 
 def transmit(event):
-	iface.indicate_pending()
-	
-	payload = {'device_id':id, 'card_swipe_value':iface.get_entry()}
-	r = session.post(card_receiver, data=payload).json()
-	print(r)
-		
-	if (r['Status'] == 'success'):
-		iface.indicate_success()
-	else:
-		iface.indicate_failure()
-		
-	
+    iface.indicate_pending()
+    
+    payload = {'device_id':id, 'card_swipe_value':iface.get_entry()}
+    r = session.post(card_receiver, data=payload).json()
+    print(r)
+        
+    if (r['Status'] == 'success'):
+        iface.indicate_success()
+    else:
+        iface.indicate_failure()
+        
+    
 def on_close():
-	config_repeater.stop.set()
-	checkin_repeater.stop.set()
-		
-	
+    config_repeater.stop.set()
+    checkin_repeater.stop.set()
+        
+    
 def detect_and_apply_config():
-	conf = get_provided_config()
-	
-	if conf:
-		iface.indicate_usb_connect()
-		
-		if config_needed():
-			perform_config()
-			iface.indicate_reboot()
-			time.sleep(5)
-			os.popen('reboot')
-	
-	else:
-		iface.indicate_no_usb()
+    conf = get_provided_config()
+    
+    if conf:
+        iface.indicate_usb_connect()
+        
+        if config_needed():
+            perform_config()
+            iface.indicate_reboot()
+            time.sleep(5)
+            os.popen('reboot')
+    
+    else:
+        iface.indicate_no_usb()
 
 
 def check_in():
-	if not is_connected():
-		iface.set_unconfigured()
-		return
-	
-	r = session.get(checkin_receiver).json()
-	
-	if r['found_open_session']:
-		iface.set_active()
-	else:
-		iface.set_idle()
-		
+    if not is_connected():
+        iface.set_unconfigured()
+        return
+    
+    r = session.get(checkin_receiver).json()
+    
+    if r['found_open_session']:
+        iface.set_active()
+    else:
+        iface.set_idle()
+        
 
 iface = Interface(is_connected())
 iface.set_on_entry(transmit)
--- repeater.py
+++ repeater.py
@@ -4,25 +4,25 @@ import threading
 import time
 
 class Repeater(threading.Thread):
-	"""
-	Class for performing a function repeatedly on a separate thread
-	"""
-	
-	def __init__(self, action, duration=1):
-		"""
-		action - function to be performed
-		duration - time in seconds to wait between function calls
-		"""
-		threading.Thread.__init__(self)
-		self.stop = threading.Event()
-		self.action = action
-		self.duration = duration
-	
-	
-	def run(self):
-		while not self.stop.is_set():
-			
-			self.action()
-			
-			time.sleep(self.duration)
+    """
+    Class for performing a function repeatedly on a separate thread
+    """
+    
+    def __init__(self, action, duration=1):
+        """
+        action - function to be performed
+        duration - time in seconds to wait between function calls
+        """
+        threading.Thread.__init__(self)
+        self.stop = threading.Event()
+        self.action = action
+        self.duration = duration
+    
+    
+    def run(self):
+        while not self.stop.is_set():
+            
+            self.action()
+            
+            time.sleep(self.duration)
 
