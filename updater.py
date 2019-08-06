5cd180255d23fa4bae1afe6d54f5bea20c9e20be
Bartosz Przybysz <bartosz.d.przybysz@gmail.com>
2019-08-06 15:22:54 -0400

    implement current_version()

--- updater.py
+++ updater.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
+from pkg_resources import parse_version
+
+
 def current_version():
-    pass
+    os.chdir('/home/pi/qwickly')
+    p = os.popen('git describe --tags')
+    version = p.read()
+    return version.strip()
 
 
 def update_available():
